68f1f6153d7b962ee602322c5230c684082c24ff
Oliver Frendo <oliver.frendo@sap.com>
2014-11-25 14:20:39 +0100

    Changed server port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ function start() {
     response.end();
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(process.env.PORT || 5000);
   console.log("Server has started.");
 }
 
