08e2d37db4e8029eeb6477110d44cdc651329a27
Oliver Frendo <oliver.frendo@sap.com>
2014-11-26 13:32:46 +0100

    ADSF

--- js/db.js
+++ js/db.js
@@ -2,13 +2,44 @@ var pg = require('pg');
 
 var db = exports;
 
-var client;
+var query = function(text, values, cb) {
+	pg.connect(function(err, client, done) {
+		client.query(text, function(err, result) {
+			done();
+			cb(err, result);
+		})
+	});
+}
+
+db.createTestTable = function(callback) {
+	query("CREATE TABLE test (num SERIAL, name varchar(20))", function(error, result) {
+		console.log("Test table created.");
+		callback();
+	});
+};
+
+db.insertTestTable = function (callback) {
+	query("INSERT INTO test (name) VALUES('Hello world!')", function(error, result) {
+		console.log("Inserted.");
+		callback();
+	});
+};
+
+db.readTestTable = function(callback) {
+	query("SELECT MAX(num) FROM test", function(error, result) {
+		console.log("Read.");
+		callback(JSON.stringify(result));
+	});
+}	
+
+/*var client;
 db.connect = function(callback) {
 	pg.connect(process.env.DATABASE_URL, function(err, c) {
 		client = c;
 		if (typeof(callback) == "function") callback();
 	});
 }
+
 
 db.createTestTable = function(callback) {
 	var sql = "CREATE TABLE test (num SERIAL, name varchar(20))";
@@ -24,6 +55,7 @@ db.insertTestTable = function (callback) {
 	var sql = "INSERT INTO test (name) VALUES('Hello world!')";
 	var query = client.query(sql);
 
+	//This gives a timeout
 	query.on("row", function(row) {
 		console.log("Row inserted.");
 		callback();
@@ -34,11 +66,12 @@ db.readTestTable = function(callback) {
 	var sql = "SELECT MAX(num) FROM test";
 	var query = client.query(sql);
 
+	//This works
 	query.on("row", function(row) {
 		console.log("Read table.")
 		console.log(row);
 		callback(JSON.stringify(row));
 	})
-};
+};*/
 
 exports.db = db;
