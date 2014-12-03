6939f3b373904899930dc711b2e1895dd74ac64b
Matthias Liedtke <mliedtke@gmx.de>
2014-12-03 15:11:00 +0100

    create table statements added

--- tableCreation.sql
+++ tableCreation.sql
@@ -1,8 +1,57 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
-  email text,
-  username text,
-  password text,
-  name text
+  email VARCHAR(100) NOT NULL,
+  username VARCHAR(30) NOT NULL,
+  password VARCHAR(32) NOT NULL,
+  salt VARCHAR(16) NOT NULL,
+  name VARCHAR(100)
+);
+
+CREATE TABLE trip (
+	trip_id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+	start_date TIMESTAMP, 
+	end_date TIMESTAMP
+);
+
+CREATE TABLE user_trip (
+	user_id INT REFERENCES users(user_id),
+	trip_id INT REFERENCES trip(trip_id)
+);
+
+CREATE TABLE destination (
+	destination_id SERIAL PRIMARY KEY,
+	trip_id  INT REFERENCES trip(trip_id),
+	name VARCHAR(50) NOT NULL,
+	place_id VARCHAR(27),
+	longitude DECIMAL(10, 7) NOT NULL,
+	latitude DECIMAL(10, 7) NOT NULL,
+	start_date TIMESTAMP,
+	end_date TIMESTAMP,
+	created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+	ranking INT
+);
+
+CREATE TABLE location (
+	location_id SERIAL PRIMARY KEY,
+	destination_id INT REFERENCES destination(destination_id),
+	name VARCHAR(50) NOT NULL,
+	place_id VARCHAR(27),
+	category VARCHAR(50),
+	longitude DECIMAL(10, 7) NOT NULL,
+	latitude DECIMAL(10, 7) NOT NULL,
+	start_date TIMESTAMP,
+	end_date TIMESTAMP,
+	created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	ranking INT
+);
+
+CREATE TABLE message (
+	message_id SERIAL PRIMARY KEY,
+	user_id INT REFERENCES users(user_id),
+	trip_id INT REFERENCES trip(trip_id),
+	msg_text TEXT NOT NULL,
+	created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
